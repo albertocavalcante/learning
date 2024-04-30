8711f4ac1e24e7e637fe26b4e6d7a73540f2f1c1
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2024-04-30 02:17:21 -0300

    update type_assertion to use is_same_v // adopt C++ 17

--- linkedin-learning/modern-c++-lambdas/closure_objects/type_assertion.cpp
+++ linkedin-learning/modern-c++-lambdas/closure_objects/type_assertion.cpp
@@ -5,7 +5,6 @@
 int main() {
     auto l1 = [](int a, int b) { return a < b; };
     auto l2 = [](int a, int b) { return a < b; };
-
-    // Note: since C++ 17 it's possible to use std::is_same_v
-    static_assert(!std::is_same<decltype(l1), decltype(l2)>::value);
+    
+    static_assert(!std::is_same_v<decltype(l1), decltype(l2)>);
 }
